396247533b496259eb17dfda505e9d8657aa0325
Educg550 <eduardo9cruz@gmail.com>
2024-08-08 08:25:24 -0300

    remove comments

--- read_image_text.py
+++ read_image_text.py
@@ -1,32 +1,7 @@
 import streamlit as st
 import os
 from dotenv import load_dotenv
-# from langchain.text_splitter import CharacterTextSplitter
-# from langchain_community.embeddings import HuggingFaceInstructEmbeddings
-# from langchain_community.vectorstores import FAISS
-# from langchain_community.chat_models import ChatOpenAI
-# from langchain_community.llms import HuggingFaceHub
-# from langchain.memory import ConversationBufferMemory
-# from langchain.chains import ConversationalRetrievalChain
-# from huggingface_hub import InferenceApi
 import google.generativeai as genai
-
-# Prompt: Based on the following text excerpt from an electricity bill, answer whether the energy company used is ENEL or CEMIG, answer just one or the other. The text is in Portuguese.
-
-# def get_text_chunks(raw_text):
-#     text_splitter = CharacterTextSplitter(
-#         separator="",
-#         chunk_size=1000,
-#         chunk_overlap=200,
-#         length_function=len
-#     )
-#     chunks = text_splitter.split_text(raw_text)
-#     return chunks
-
-# def get_vectorstore(text_chunks):
-#     embeddings = HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-xl')
-#     vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-#     return vector_store
 
 def get_llm_model():
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
@@ -54,10 +29,6 @@ def main():
                 if uploaded_file is not None:
                     bytes_text = uploaded_file.read()
                     raw_text = bytes_text.decode("utf-8")
-
-                    # text_chunks = get_text_chunks(raw_text)
-                    
-                    # vector_store = get_vectorstore(text_chunks)
 
                     conversation = get_llm_model()
 
